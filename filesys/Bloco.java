7c6ff5d9f2b019fec5491453350fa433717fdfff
Mateus <mateuaraujo01@gmail.com>
2025-06-07 15:57:03 -0300

    criando classe bloco

--- filesys/Bloco.java
+++ filesys/Bloco.java
@@ -1,5 +1,17 @@
 package filesys;
 
 public class Bloco {
-    
+    byte[] bytes;
+
+    public Bloco(){
+        bytes = new byte[4096];
+    }
+
+    public byte[] getDados(){
+        return bytes;
+    }
+
+    public void setBytes(byte[] bytes){
+        this.bytes = bytes;
+    }
 }
