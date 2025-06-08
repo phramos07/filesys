4ffcf7f70cf45a42ce93b7712788283449e1dde3
Mateus <mateuaraujo01@gmail.com>
2025-06-08 09:54:52 -0300

    criando metadados

---
+++ filesys/MetaDados.java
@@ -0,0 +1,35 @@
+package filesys;
+
+/*classe base que tem o nome, permissões básicas e o dono, tanto Diretorio quanto Arquivo a implementam */
+public class MetaDados {
+    
+    protected String nome;
+    protected String permissoesBasicas;
+    protected String dono;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getPermissoesBasicas() {
+        return permissoesBasicas;
+    }
+    public void setPermissoesBasicas(String permissoesBasicas) {
+        this.permissoesBasicas = permissoesBasicas;
+    }
+    public String getDono() {
+        return dono;
+    }
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public MetaDados(String nome, String permissoesBasicas, String dono) {
+        setNome(nome);
+        setPermissoesBasicas(permissoesBasicas);
+        setDono(dono);
+    }
+       
+}
