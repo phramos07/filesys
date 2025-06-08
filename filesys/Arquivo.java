8168757f7967f18df852d6f2bda2c136788ab05c
Mateus <mateuaraujo01@gmail.com>
2025-06-08 09:54:37 -0300

    criando arquivo

--- filesys/Arquivo.java
+++ filesys/Arquivo.java
@@ -2,15 +2,17 @@ package filesys;
 import exception.BlocoVazioException;
 import java.util.List;
 
-public class Arquivo extends Diretorio {
+public class Arquivo extends MetaDados {
+
     List<Bloco> blocos;
     private long tamanho;
 
+    //adiciona bloco na lista e incrementa o tamanho atual
     public void adicionarBloco(Bloco bloco){
         blocos.add(bloco);
         tamanho++;
     }
-
+    //remove blocos da lista por posição passada e decrementa o tamanho atual
     public Bloco removerBlocoPos(int pos){
         if(blocos.isEmpty()){
             throw new BlocoVazioException("Não é possível remover pois o bloco já está vazio");
@@ -18,7 +20,7 @@ public class Arquivo extends Diretorio {
         tamanho--;
         return blocos.remove(pos);
     }
-
+    //limpa o arquivo deletando tudo que está lá e iguala tamanho atual a 0
     public void limparArquivo(){
         if(blocos.isEmpty())
             throw new BlocoVazioException("Não é possível limpar pois o bloco já está vazio");
@@ -37,6 +39,12 @@ public class Arquivo extends Diretorio {
     public long getTamanhoAtual(){
         return tamanho;
     }
+    public Arquivo(String nome, String permissoesBasicas, String dono, List<Bloco> blocos, long tamanho) {
+        super(nome, permissoesBasicas, dono);
+        this.blocos = blocos;
+        this.tamanho = tamanho;
+    }
 
+    
 
 }
