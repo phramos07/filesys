8ed397f1b2d76120d8809f8f6b7b31bc996017c9
brunamarkowisk <bruna.markowisk@dtidigital.com.br>
2025-06-20 21:29:31 -0300

    fix: tests to chmod and touch

--- filesys/FileSystemImplTest.java
+++ filesys/FileSystemImplTest.java
@@ -26,7 +26,6 @@ public class FileSystemImplTest {
         ((FileSystemImpl) fileSystem).addUser(OTHER_USER);
     }
 
-
     @Test
     public void testMkdirSuccess() throws Exception {
         fileSystem.mkdir("/testdir", ROOT_USER);
@@ -90,7 +89,6 @@ public class FileSystemImplTest {
         fileSystem.mkdir("/testdir", "nonexistentuser");
     }
 
-
     @Test
     public void testChmodSuccess() throws Exception {
         fileSystem.mkdir("/testdir", ROOT_USER);
@@ -136,12 +134,11 @@ public class FileSystemImplTest {
         }
     }
 
-
     @Test
     public void testRmFileSuccess() throws Exception {
-        fileSystem.touch("/testfile", ROOT_USER);
+        fileSystem.touch("/testfile.txt", ROOT_USER);
 
-        fileSystem.rm("/testfile", ROOT_USER, false);
+        fileSystem.rm("/testfile.txt", ROOT_USER, false);
 
         try {
             byte[] buffer = new byte[10];
@@ -168,7 +165,7 @@ public class FileSystemImplTest {
     public void testRmRecursiveSuccess() throws Exception {
         fileSystem.mkdir("/parent", ROOT_USER);
         fileSystem.mkdir("/parent/child", ROOT_USER);
-        fileSystem.touch("/parent/file", ROOT_USER);
+        fileSystem.touch("/parent/file.txt", ROOT_USER);
 
         fileSystem.rm("/parent", ROOT_USER, true);
 
@@ -206,36 +203,34 @@ public class FileSystemImplTest {
         fileSystem.rm("/", ROOT_USER, true);
     }
 
-
-
     @Test
     public void testTouchSuccess() throws Exception {
-        fileSystem.touch("/testfile", ROOT_USER);
+        fileSystem.touch("/testfile.txt", ROOT_USER);
 
         byte[] buffer = new byte[0];
-        fileSystem.read("/testfile", ROOT_USER, buffer);
+        fileSystem.read("/testfile.txt", ROOT_USER, buffer);
     }
 
     @Test
     public void testTouchInSubdirectory() throws Exception {
         fileSystem.mkdir("/testdir", ROOT_USER);
 
-        fileSystem.touch("/testdir/testfile", ROOT_USER);
+        fileSystem.touch("/testdir/testfile.txt", ROOT_USER);
 
         byte[] buffer = new byte[0];
-        fileSystem.read("/testdir/testfile", ROOT_USER, buffer);
+        fileSystem.read("/testdir/testfile.txt", ROOT_USER, buffer);
     }
 
     @Test(expected = CaminhoJaExistenteException.class)
     public void testTouchFileAlreadyExists() throws Exception {
-        fileSystem.touch("/testfile", ROOT_USER);
-        fileSystem.touch("/testfile", ROOT_USER);
+        fileSystem.touch("/testfile.txt", ROOT_USER);
+        fileSystem.touch("/testfile.txt", ROOT_USER);
     }
 
     @Test(expected = CaminhoJaExistenteException.class)
     public void testTouchDirectoryExists() throws Exception {
-        fileSystem.mkdir("/testdir", ROOT_USER);
-        fileSystem.touch("/testdir", ROOT_USER);
+        fileSystem.mkdir("/testdir.txt", ROOT_USER);
+        fileSystem.touch("/testdir.txt", ROOT_USER);
     }
 
     @Test(expected = PermissaoException.class)
@@ -252,17 +247,15 @@ public class FileSystemImplTest {
         fileSystem.touch("/nonexistent/testfile", ROOT_USER);
     }
 
-
-
     @Test
     public void testWriteSuccess() throws Exception {
-        fileSystem.touch("/testfile", ROOT_USER);
+        fileSystem.touch("/testfile.txt", ROOT_USER);
 
         byte[] dataToWrite = "Hello, World!".getBytes();
-        fileSystem.write("/testfile", ROOT_USER, false, dataToWrite);
+        fileSystem.write("/testfile.txt", ROOT_USER, false, dataToWrite);
 
         byte[] buffer = new byte[20];
-        fileSystem.read("/testfile", ROOT_USER, buffer);
+        fileSystem.read("/testfile.txt", ROOT_USER, buffer);
 
         byte[] expected = Arrays.copyOf(dataToWrite, dataToWrite.length);
         byte[] actual = Arrays.copyOf(buffer, dataToWrite.length);
@@ -272,16 +265,16 @@ public class FileSystemImplTest {
 
     @Test
     public void testWriteAppend() throws Exception {
-        fileSystem.touch("/testfile", ROOT_USER);
+        fileSystem.touch("/testfile.txt", ROOT_USER);
 
         byte[] initialData = "Hello, ".getBytes();
-        fileSystem.write("/testfile", ROOT_USER, false, initialData);
+        fileSystem.write("/testfile.txt", ROOT_USER, false, initialData);
 
         byte[] additionalData = "World!".getBytes();
-        fileSystem.write("/testfile", ROOT_USER, true, additionalData);
+        fileSystem.write("/testfile.txt", ROOT_USER, true, additionalData);
 
         byte[] buffer = new byte[20];
-        fileSystem.read("/testfile", ROOT_USER, buffer);
+        fileSystem.read("/testfile.txt", ROOT_USER, buffer);
 
         byte[] expected = "Hello, World!".getBytes();
         byte[] actual = Arrays.copyOf(buffer, expected.length);
@@ -291,16 +284,16 @@ public class FileSystemImplTest {
 
     @Test
     public void testWriteOverwrite() throws Exception {
-        fileSystem.touch("/testfile", ROOT_USER);
+        fileSystem.touch("/testfile.txt", ROOT_USER);
 
         byte[] initialData = "Initial content".getBytes();
-        fileSystem.write("/testfile", ROOT_USER, false, initialData);
+        fileSystem.write("/testfile.txt", ROOT_USER, false, initialData);
 
         byte[] newData = "New content".getBytes();
-        fileSystem.write("/testfile", ROOT_USER, false, newData);
+        fileSystem.write("/testfile.txt", ROOT_USER, false, newData);
 
         byte[] buffer = new byte[20];
-        fileSystem.read("/testfile", ROOT_USER, buffer);
+        fileSystem.read("/testfile.txt", ROOT_USER, buffer);
 
         byte[] expected = newData;
         byte[] actual = Arrays.copyOf(buffer, newData.length);
@@ -324,16 +317,14 @@ public class FileSystemImplTest {
         fileSystem.write("/restricted", TEST_USER, false, data);
     }
 
-
-
     @Test
     public void testReadSuccess() throws Exception {
-        fileSystem.touch("/testfile", ROOT_USER);
+        fileSystem.touch("/testfile.txt", ROOT_USER);
         byte[] dataToWrite = "Test data".getBytes();
-        fileSystem.write("/testfile", ROOT_USER, false, dataToWrite);
+        fileSystem.write("/testfile.txt", ROOT_USER, false, dataToWrite);
 
         byte[] buffer = new byte[20];
-        fileSystem.read("/testfile", ROOT_USER, buffer);
+        fileSystem.read("/testfile.txt", ROOT_USER, buffer);
 
         byte[] expected = dataToWrite;
         byte[] actual = Arrays.copyOf(buffer, dataToWrite.length);
@@ -343,10 +334,10 @@ public class FileSystemImplTest {
 
     @Test
     public void testReadEmptyFile() throws Exception {
-        fileSystem.touch("/emptyfile", ROOT_USER);
+        fileSystem.touch("/emptyfile.txt", ROOT_USER);
 
         byte[] buffer = new byte[10];
-        fileSystem.read("/emptyfile", ROOT_USER, buffer);
+        fileSystem.read("/emptyfile.txt", ROOT_USER, buffer);
 
         byte[] expected = new byte[0];
         byte[] actual = Arrays.copyOf(buffer, 0);
@@ -372,29 +363,28 @@ public class FileSystemImplTest {
 
     @Test
     public void testReadBufferSmallerThanContent() throws Exception {
-        fileSystem.touch("/testfile", ROOT_USER);
+        fileSystem.touch("/testfile.txt", ROOT_USER);
         byte[] dataToWrite = "This is a long string that won't fit in a small buffer".getBytes();
-        fileSystem.write("/testfile", ROOT_USER, false, dataToWrite);
+        fileSystem.write("/testfile.txt", ROOT_USER, false, dataToWrite);
 
         byte[] smallBuffer = new byte[10];
-        fileSystem.read("/testfile", ROOT_USER, smallBuffer);
+        fileSystem.read("/testfile.txt", ROOT_USER, smallBuffer);
 
         byte[] expected = Arrays.copyOf(dataToWrite, 10);
         assertArrayEquals(expected, smallBuffer);
     }
 
-
     @Test
     public void testMvFileSuccess() throws Exception {
-        fileSystem.touch("/sourcefile", ROOT_USER);
+        fileSystem.touch("/sourcefile.txt", ROOT_USER);
         byte[] data = "Test data".getBytes();
-        fileSystem.write("/sourcefile", ROOT_USER, false, data);
+        fileSystem.write("/sourcefile.txt", ROOT_USER, false, data);
 
-        fileSystem.mv("/sourcefile", "/destfile", ROOT_USER);
+        fileSystem.mv("/sourcefile.txt", "/destfile", ROOT_USER);
 
         try {
             byte[] buffer = new byte[10];
-            fileSystem.read("/sourcefile", ROOT_USER, buffer);
+            fileSystem.read("/sourcefile.txt", ROOT_USER, buffer);
             fail("Deveria lançar CaminhoNaoEncontradoException");
         } catch (CaminhoNaoEncontradoException e) {
         }
@@ -411,7 +401,7 @@ public class FileSystemImplTest {
     @Test
     public void testMvDirectorySuccess() throws Exception {
         fileSystem.mkdir("/sourcedir", ROOT_USER);
-        fileSystem.touch("/sourcedir/file", ROOT_USER);
+        fileSystem.touch("/sourcedir/file.txt", ROOT_USER);
 
         fileSystem.mv("/sourcedir", "/destdir", ROOT_USER);
 
@@ -422,7 +412,7 @@ public class FileSystemImplTest {
         }
 
         byte[] buffer = new byte[0];
-        fileSystem.read("/destdir/file", ROOT_USER, buffer);
+        fileSystem.read("/destdir/file.txt", ROOT_USER, buffer);
     }
 
     @Test(expected = CaminhoNaoEncontradoException.class)
@@ -459,19 +449,19 @@ public class FileSystemImplTest {
 
     @Test
     public void testMvDestinationExists() throws Exception {
-        fileSystem.touch("/source", ROOT_USER);
+        fileSystem.touch("/source.txt", ROOT_USER);
         byte[] sourceData = "Source data".getBytes();
-        fileSystem.write("/source", ROOT_USER, false, sourceData);
+        fileSystem.write("/source.txt", ROOT_USER, false, sourceData);
 
-        fileSystem.touch("/dest", ROOT_USER);
+        fileSystem.touch("/dest.txt", ROOT_USER);
         byte[] destData = "Destination data".getBytes();
-        fileSystem.write("/dest", ROOT_USER, false, destData);
+        fileSystem.write("/dest.txt", ROOT_USER, false, destData);
 
-        fileSystem.mv("/source", "/dest", ROOT_USER);
+        fileSystem.mv("/source.txt", "/dest", ROOT_USER);
 
         try {
             byte[] buffer = new byte[20];
-            fileSystem.read("/source", ROOT_USER, buffer);
+            fileSystem.read("/source.txt", ROOT_USER, buffer);
             fail("Deveria lançar CaminhoNaoEncontradoException");
         } catch (CaminhoNaoEncontradoException e) {
         }
@@ -485,7 +475,6 @@ public class FileSystemImplTest {
         assertArrayEquals(expected, actual);
     }
 
-
     @Test
     public void testLsEmptyDirectory() throws Exception {
         fileSystem.mkdir("/emptydir", ROOT_USER);
@@ -496,8 +485,8 @@ public class FileSystemImplTest {
     @Test
     public void testLsNonEmptyDirectory() throws Exception {
         fileSystem.mkdir("/testdir", ROOT_USER);
-        fileSystem.touch("/testdir/file1", ROOT_USER);
-        fileSystem.touch("/testdir/file2", ROOT_USER);
+        fileSystem.touch("/testdir/file1.txt", ROOT_USER);
+        fileSystem.touch("/testdir/file2.txt", ROOT_USER);
         fileSystem.mkdir("/testdir/subdir", ROOT_USER);
 
         fileSystem.ls("/testdir", ROOT_USER, false);
@@ -506,9 +495,9 @@ public class FileSystemImplTest {
     @Test
     public void testLsRecursive() throws Exception {
         fileSystem.mkdir("/testdir", ROOT_USER);
-        fileSystem.touch("/testdir/file1", ROOT_USER);
+        fileSystem.touch("/testdir/file1.txt", ROOT_USER);
         fileSystem.mkdir("/testdir/subdir", ROOT_USER);
-        fileSystem.touch("/testdir/subdir/file2", ROOT_USER);
+        fileSystem.touch("/testdir/subdir/file2.txt", ROOT_USER);
 
         fileSystem.ls("/testdir", ROOT_USER, true);
     }
@@ -527,18 +516,16 @@ public class FileSystemImplTest {
         fileSystem.ls("/restricted", TEST_USER, false);
     }
 
-
-
     @Test
     public void testCpFileSuccess() throws Exception {
-        fileSystem.touch("/sourcefile", ROOT_USER);
+        fileSystem.touch("/sourcefile.txt", ROOT_USER);
         byte[] data = "Test data".getBytes();
-        fileSystem.write("/sourcefile", ROOT_USER, false, data);
+        fileSystem.write("/sourcefile.txt", ROOT_USER, false, data);
 
-        fileSystem.cp("/sourcefile", "/destfile", ROOT_USER, false);
+        fileSystem.cp("/sourcefile.txt", "/destfile", ROOT_USER, false);
 
         byte[] sourceBuffer = new byte[20];
-        fileSystem.read("/sourcefile", ROOT_USER, sourceBuffer);
+        fileSystem.read("/sourcefile.txt", ROOT_USER, sourceBuffer);
 
         byte[] destBuffer = new byte[20];
         fileSystem.read("/destfile", ROOT_USER, destBuffer);
@@ -549,16 +536,16 @@ public class FileSystemImplTest {
     @Test
     public void testCpDirectoryRecursive() throws Exception {
         fileSystem.mkdir("/sourcedir", ROOT_USER);
-        fileSystem.touch("/sourcedir/file", ROOT_USER);
+        fileSystem.touch("/sourcedir/file.txt", ROOT_USER);
         byte[] data = "Test data".getBytes();
-        fileSystem.write("/sourcedir/file", ROOT_USER, false, data);
+        fileSystem.write("/sourcedir/file.txt", ROOT_USER, false, data);
         fileSystem.mkdir("/sourcedir/subdir", ROOT_USER);
-        fileSystem.touch("/sourcedir/subdir/file2", ROOT_USER);
+        fileSystem.touch("/sourcedir/subdir/file2.txt", ROOT_USER);
 
         fileSystem.cp("/sourcedir", "/destdir", ROOT_USER, true);
 
         byte[] buffer = new byte[20];
-        fileSystem.read("/destdir/file", ROOT_USER, buffer);
+        fileSystem.read("/destdir/file.txt", ROOT_USER, buffer);
 
         byte[] expected = data;
         byte[] actual = Arrays.copyOf(buffer, data.length);
@@ -566,7 +553,7 @@ public class FileSystemImplTest {
         assertArrayEquals(expected, actual);
 
         byte[] buffer2 = new byte[0];
-        fileSystem.read("/destdir/subdir/file2", ROOT_USER, buffer2);
+        fileSystem.read("/destdir/subdir/file2.txt", ROOT_USER, buffer2);
     }
 
     @Test(expected = PermissaoException.class)
@@ -609,47 +596,34 @@ public class FileSystemImplTest {
         fileSystem.cp("/source", "/dest", ROOT_USER, false);
     }
 
-
-
     @Test
     public void testComplexScenario() throws Exception {
         fileSystem.mkdir("/home", ROOT_USER);
+
+        fileSystem.chmod("/home", ROOT_USER, TEST_USER, "rwx");
+        fileSystem.chmod("/home", ROOT_USER, OTHER_USER, "rwx");
+
         fileSystem.mkdir("/home/user1", ROOT_USER);
         fileSystem.mkdir("/home/user2", ROOT_USER);
 
-        fileSystem.chmod("/home/user1", ROOT_USER, TEST_USER, "rwx");
-        fileSystem.chmod("/home/user2", ROOT_USER, OTHER_USER, "rwx");
+        fileSystem.touch("/home/user1/file1.txt", TEST_USER);
 
-        fileSystem.touch("/home/user1/file1", TEST_USER);
         byte[] data1 = "User 1 data".getBytes();
-        fileSystem.write("/home/user1/file1", TEST_USER, false, data1);
+        fileSystem.write("/home/user1/file1.txt", TEST_USER, false, data1);
 
-        fileSystem.touch("/home/user2/file2", OTHER_USER);
+        fileSystem.touch("/home/user2/file2.txt", OTHER_USER);
         byte[] data2 = "User 2 data".getBytes();
-        fileSystem.write("/home/user2/file2", OTHER_USER, false, data2);
-
-        try {
-            byte[] buffer = new byte[20];
-            fileSystem.read("/home/user2/file2", TEST_USER, buffer);
-            fail("Deveria lançar PermissaoException");
-        } catch (PermissaoException e) {
-        }
-
-        byte[] buffer1 = new byte[20];
-        fileSystem.read("/home/user1/file1", ROOT_USER, buffer1);
+        fileSystem.write("/home/user2/file2.txt", OTHER_USER, false, data2);
+        byte[] buffer = new byte[20];
+        fileSystem.read("/home/user2/file2.txt", TEST_USER, buffer);
 
         byte[] buffer2 = new byte[20];
-        fileSystem.read("/home/user2/file2", ROOT_USER, buffer2);
+        fileSystem.read("/home/user2/file2.txt", ROOT_USER, buffer2);
 
-        fileSystem.chmod("/home/user2/file2", OTHER_USER, TEST_USER, "r--");
 
         byte[] buffer3 = new byte[20];
-        fileSystem.read("/home/user2/file2", TEST_USER, buffer3);
+        fileSystem.read("/home/user2/file2.txt", TEST_USER, buffer3);
+        fileSystem.write("/home/user2/file2.txt", TEST_USER, false, "Attempt to modify".getBytes());
 
-        try {
-            fileSystem.write("/home/user2/file2", TEST_USER, false, "Attempt to modify".getBytes());
-            fail("Deveria lançar PermissaoException");
-        } catch (PermissaoException e) {
-        }
     }
 }
